9e65a1967229721277ac447c50b85df22abf313d
dicleyamanoglu <37039118+dicleyamanoglu@users.noreply.github.com>
2018-10-08 20:17:17 +0300

    Add files via upload

---
+++ odev.py
@@ -0,0 +1,231 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 14, in <module>
+    yatayKutu.addWidget(buton)
+NameError: name 'buton' is not defined
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 9, in <module>
+    yatayKutu=addWidget(etiket)
+NameError: name 'addWidget' is not defined
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 11, in <module>
+    cevap=QInput
+NameError: name 'QInput' is not defined
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 11, in <module>
+    cevap=QEditLine
+NameError: name 'QEditLine' is not defined
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 11, in <module>
+    self.vizeAdeger = QLineEdit()
+NameError: name 'self' is not defined
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 9, in <module>
+    textbox = QLineEdit(etiket1)
+NameError: name 'etiket1' is not defined
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 20, in <module>
+    yatayKutu.addWidget(etiket)
+NameError: name 'yatayKutu' is not defined
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 25, in <module>
+    textbox.addWidget(etiket)
+AttributeError: 'QLineEdit' object has no attribute 'addWidget'
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+Traceback (most recent call last):
+  File "C:\Users\Dicle\Desktop\dd.py", line 15, in <module>
+    textbox.move(0,100,0,100)
+TypeError: arguments did not match any overloaded call:
+  QWidget.move(QPoint): argument 1 has unexpected type 'int'
+  QWidget.move(int, int): too many arguments
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
+=================== RESTART: C:\Users\Dicle\Desktop\dd.py ===================
+>>> 
